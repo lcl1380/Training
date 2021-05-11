0b3c7c48835a81c7d996ba884e40de1cdc975fea
Chae rin <49014223+lcl1380@users.noreply.github.com>
2021-05-11 10:15:30 +0900

    [ C ] 도형 크기 변경
    b 입력하면 도형의 크기를 크게
    s 입력하면 도형의 크기를 작게
    q 입력하면 프로그램 중지

---
+++ [ C ] 도형 크기 변경.c
@@ -0,0 +1,24 @@
+#include <windows.h>
+#include <stdio.h>
+int main(void) {
+	int ch;  //정수형 주의
+	int width = 200, height = 200;
+	HDC hdc = GetWindowDC(GetForegroundWindow());
+	while (1) {//while(1)은 무한반복문
+		Rectangle(hdc, 100, 100, width, height);
+		ch = getchar();
+		Rectangle(hdc, 0, 0, 400, 300);
+		if (ch == 'b') {
+			width += 10; //width가 10 증가함
+			height += 10; //height가 10 증가함
+		}
+		else if (ch == 's') {
+			width -= 10;
+			height -= 10;
+		}
+		else if (ch == 'q') {
+			return 0;
+		}
+	}
+	return 0;
+} //근데 계속해서 커지진 않음 뭐임 ㅜ
